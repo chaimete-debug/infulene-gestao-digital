fcf6adbd20b8bbbd7c26321bb901f4f23321feb8
chaimete-debug <chaimete@gmail.com>
2026-05-26 00:56:13 +0200

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-window.APP_CONFIG = {
-  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbzqEqge_wauXLg5FHX5eyZdEdU6KrghnXL1SJzY6d4IX7RCjvrvhQV3OrTQ3wIDsASW/exec',
-  APP_NAME: 'Sistema de Registos da Igreja',
-  ORG_NAME: 'Igreja Local',
-  DEFAULT_PIN_ENABLED: true
-};
